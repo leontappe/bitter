faa10dc06457ddc2aba48233f06d015b06907847
Leon Tappe <ltappe@mail.uni-paderborn.de>
2020-03-27 01:18:50 +0100

    add model for storing mysql conn settings

---
+++ lib/src/models/mysql_settings.dart
@@ -0,0 +1,38 @@
+import 'package:meta/meta.dart';
+
+class MySqlSettings {
+  String host;
+  int port;
+  String user;
+  String password;
+  String database;
+
+  MySqlSettings({
+    this.host = '127.0.0.1',
+    this.port = 3306,
+    @required this.user,
+    @required this.password,
+    @required this.database,
+  });
+
+  factory MySqlSettings.empty() => MySqlSettings(user: null, password: null, database: null);
+
+  factory MySqlSettings.fromMap(Map map) => MySqlSettings(
+        host: map['host'].toString(),
+        port: map['port'] as int,
+        user: map['user'].toString(),
+        password: map['password'].toString(),
+        database: map['database'].toString(),
+      );
+
+  Map<String, dynamic> get toMap => <String, dynamic>{
+        'host': host,
+        'port': port,
+        'user': user,
+        'password': password,
+        'database': database,
+      };
+
+  @override
+  String toString() => '[MySqlSettings $toMap]';
+}
