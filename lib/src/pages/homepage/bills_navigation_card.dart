8f065b522badbca39ce962aee4bc4cb09024ea52
Leon Tappe <ltappe@mail.uni-paderborn.de>
2020-09-18 21:11:45 +0200

    add overview of relevant bills on homepage

--- lib/src/pages/homepage/bills_navigation_card.dart
+++ lib/src/pages/homepage/bills_navigation_card.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../models/bill.dart';
+import '../../providers/database_provider.dart';
+import '../../providers/inherited_database.dart';
+import '../../repositories/bill_repository.dart';
+import '../../widgets/bill_shortcut.dart';
 import '../../widgets/navigation_card.dart';
 
 class BillsNavigationCard extends StatefulWidget {
@@ -8,6 +13,9 @@ class BillsNavigationCard extends StatefulWidget {
 }
 
 class _BillsNavigationCardState extends State<BillsNavigationCard> {
+  BillRepository _billRepo;
+  List<Bill> _bills = [];
+
   @override
   Widget build(BuildContext context) {
     return NavigationCard(
@@ -15,7 +23,63 @@ class _BillsNavigationCardState extends State<BillsNavigationCard> {
       '/bills',
       children: <Widget>[
         Text('Rechnungen', style: Theme.of(context).textTheme.headline3),
+        Divider(height: 24.0),
+        Text('Neu', style: Theme.of(context).textTheme.headline4),
+        Flexible(
+            child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ..._bills.take(4).map<Widget>(
+                  (Bill b) => Expanded(child: BillShortcut(context, bill: b)),
+                ),
+            if (_bills.length > 4)
+              Center(child: Icon(Icons.more_horiz, color: Colors.grey, size: 48.0)),
+          ],
+        )),
+        Divider(height: 24.0),
+        Text('Überfällig', style: Theme.of(context).textTheme.headline4),
+        Flexible(
+            child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ..._bills
+                .where((Bill b) =>
+                    ((b.reminders == null || b.reminders.isEmpty) &&
+                        DateTime.now().isAfter(b.dueDate)) ||
+                    (b.reminders != null &&
+                        b.reminders.isNotEmpty &&
+                        DateTime.now().isAfter(b.reminders.last.deadline)))
+                .take(4)
+                .map<Widget>(
+                  (Bill b) => Expanded(child: BillShortcut(context, bill: b)),
+                ),
+            if (_bills.length > 4)
+              Center(child: Icon(Icons.more_horiz, color: Colors.grey, size: 48.0)),
+          ],
+        )),
       ],
     );
+  }
+
+  @override
+  void didChangeDependencies() {
+    initDb();
+    super.didChangeDependencies();
+  }
+
+  Future<void> initDb() async {
+    _billRepo = BillRepository(InheritedDatabase.of<DatabaseProvider>(context).provider);
+    await _billRepo.setUp();
+
+    await onGetBills();
+  }
+
+  Future<void> onGetBills() async {
+    _bills = await _billRepo.select();
+    _bills.sort((Bill a, Bill b) => b.created.compareTo(a.created));
+    setState(() => _bills);
+    return;
   }
 }
---
+++ lib/src/widgets/bill_shortcut.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../models/bill.dart';
+import '../pages/bills/bill_page.dart';
+
+class BillShortcut extends StatelessWidget {
+  final BuildContext context;
+  final Bill bill;
+
+  const BillShortcut(this.context, {Key key, @required this.bill}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: InkWell(
+            onTap: () => Navigator.push<bool>(context,
+                MaterialPageRoute(builder: (BuildContext context) => BillPage(id: bill.id))),
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(bill.billNr,
+                      style: Theme.of(context).textTheme.subtitle1, textScaleFactor: 1.1),
+                  Text(bill.vendor.name),
+                  Text(bill.customer.fullCompany ?? bill.customer.fullName),
+                  Text((bill.sum / 100.0).toStringAsFixed(2) + ' €'),
+                ],
+              ),
+            )));
+  }
+}
